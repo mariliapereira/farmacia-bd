a509099aa6bf1245747deedc8dc8d1c65efd0f00
Marília Pereira <msap@cesar.school>
2023-11-16 19:55:23 -0300

    login updated

--- functions.py
+++ functions.py
@@ -1,56 +1,75 @@
 import streamlit as st
-import pandas as pd
 import mysql.connector
 
-#conexão com o banco
-def open_conn(input_user, input_password):
-    global conn
-    conn = mysql.connector.connect(
-    host="localhost",
-    user= input_user,
-    password= input_password, 
-    database="UNIVERSIDADE"
-)
-    global cursor 
-    cursor = conn.cursor()
-    
+# pip install mysql-connector-python
 
-#insert
-def insert_data(table, data):
+
+# conexão com o banco
+def open_conn(input_user, input_password):
+    conn2 = mysql.connector.connect(
+        host="localhost", user="root", password="pacienc1@", database="UNIVERSIDADE"
+    )
+    cursor2 = conn2.cursor()
+    cursor2.execute(f"SELECT COUNT(*) FROM mysql.user WHERE user = '{input_user}'")
+    user_exists = cursor2.fetchone()[0]
+    if not user_exists:
+        cursor2.execute(
+            f"CREATE USER '{input_user}'@'localhost' IDENTIFIED BY '{input_password}';"
+        )
+        cursor2.execute(f"GRANT ALL PRIVILEGES ON * . * TO '{input_user}'@'localhost';")
+        cursor2.execute(f"FLUSH PRIVILEGES;")
+
+    conn = mysql.connector.connect(
+        host="localhost",
+        user=input_user,
+        password=input_password,
+        database="UNIVERSIDADE",
+    )
+
+    cursor = conn.cursor()
+    return conn, cursor
+
+# insert
+def insert_data(conn, cursor, table, data):
     query = f"INSERT INTO {table} VALUES ({', '.join(['%s']*len(data))})"
     cursor.execute(query, data)
     conn.commit()
-    st.success(f'Dados inseridos em {table}.')
+    st.success(f"Dados inseridos em {table}.")
 
-#delete
-def delete_data(table, condition_column, condition_value):
+
+# delete
+def delete_data(conn, cursor, table, condition_column, condition_value):
     query = f"DELETE FROM {table} WHERE {condition_column} = %s"
     cursor.execute(query, (condition_value,))
     conn.commit()
-    st.success(f'Dados deletados de {table}.')
+    st.success(f"Dados deletados de {table}.")
 
-#update
-def update_data(table, set_column, set_value, condition_column, condition_value):
+
+# update
+def update_data(conn, cursor, table, set_column, set_value, condition_column, condition_value):
     query = f"UPDATE {table} SET {set_column} = %s WHERE {condition_column} = %s"
     cursor.execute(query, (set_value, condition_value))
     conn.commit()
-    st.success(f'Dados atualizados em {table}.')
+    st.success(f"Dados atualizados em {table}.")
 
-#select
-#def select_data(table, data):
-    #query = f"SELECT * FROM {table}"           
-    #cursor.execute(query)
-    #data = cursor.fetchall()
-    #return data
 
-def select_data(data):
+# select
+# def select_data(conn, cursor, table, data):
+# query = f"SELECT * FROM {table}"
+# cursor.execute(query)
+# data = cursor.fetchall()
+# return data
+
+
+def select_data(conn, cursor, data):
     query = f"SELECT total_creditos({data});"
     cursor.execute(query)
     query_result = cursor.fetchone()
     valor = query_result[0]
     return valor
 
-#fechar conexão
-def close_conn():
+
+# fechar conexão
+def close_conn(conn, cursor):
     cursor.close()
     conn.close()
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import streamlit as st  
 import pandas as pd
 from functions import *
-# pip install mysql-connector-python
 
 # rodar com:  
 # streamlit run "c:/Users/heyda/OneDrive/Documentos/GitHub/farmacia-bd/main.py"
@@ -10,46 +9,64 @@ def main_page():
     st.title("Aplicação de Gerenciamento de Dados")
     operation = st.sidebar.selectbox("Selecione a operação", ("Inserir", "Deletar", "Atualizar", "Select"))
     table = st.sidebar.selectbox("Selecione a tabela", ("curso", "projeto", "pessoa", "professor", "disciplina", "turma", "ministra", "aluno", "aluno_turma", "prova", "monitoria", "busca - créditos totais"))
+    conn, cursor = open_conn(st.session_state['username'], st.session_state['password'])
 
     if operation == "Inserir":
         if table == "curso":
             codigo_curso = st.number_input("Código do Curso", min_value=0)
             nome_curso = st.text_input("Nome do Curso")
             if st.button("Inserir"):
-                insert_data(table, (codigo_curso, nome_curso))
+                insert_data(conn, cursor, table, (codigo_curso, nome_curso))
     elif operation == "Deletar":
         if table == "curso":
             codigo_curso = st.number_input("Código do Curso a ser deletado", min_value=0)
             if st.button("Deletar"):
-                delete_data(table, "codigo_curso", codigo_curso)
+                delete_data(conn, cursor, table, "codigo_curso", codigo_curso)
     elif operation == "Atualizar":
         if table == "curso":
             codigo_curso = st.number_input("Código do Curso a ser atualizado", min_value=0)
             novo_nome = st.text_input("Novo Nome do Curso")
             if st.button("Atualizar"):
-                update_data(table, "nome", novo_nome, "codigo_curso", codigo_curso)
+                update_data(conn, cursor, table, "nome", novo_nome, "codigo_curso", codigo_curso)
     elif operation == 'Select':
         if table == "curso":
-            data = select_data(table)
+            data = select_data(conn, cursor, table)
             st.write("Dados da tabela Curso:")
             df = pd.DataFrame(data, columns=["Código do Curso", "Nome do Curso"])
             st.dataframe(df.set_index('Código do Curso'), width=800)
         if table == 'aluno':
-            data = select_data(table)
+            data = select_data(conn, cursor, table)
             st.write("Dados da tabela Aluno:")
             df = pd.DataFrame(data, columns=["Matrícula do aluno", "Nota do vestibular", "Codigo do curso"])
             st.dataframe(df.set_index('Matrícula do aluno'), width=800)
         if table == 'busca - créditos totais':
             matricula_aluno = st.number_input("Matrícula do aluno", min_value=0)
             if st.button("Buscar"):
-                result = select_data(matricula_aluno)
+                result = select_data(conn, cursor, matricula_aluno)
                 st.write(f"Total de créditos do aluno: {result}")
 
-if __name__ == '__main__':
+    close_conn(conn, cursor)
+
+def login_page():
     st.title("Login")
     user = st.text_input("Usuário")
     password = st.text_input("Senha", type="password")
-    if st.button("Login"):
-        open_conn(user, password)
+    def connect():
+        try:
+            conn, cursor = open_conn(user, password)
+            st.success("Conexão estabelecida com sucesso!")
+            close_conn(conn, cursor)
+            st.session_state.username = user
+            st.session_state.password = password
+        except Exception as e:
+            st.write("Erro ao conectar: Usuário ou senha incorretos.")
+            pass
+    login_button = st.button("Login", on_click=connect)
+
+if __name__ == '__main__':
+    if 'username' not in st.session_state:
+        st.session_state.username = None
+    if st.session_state.username == None:
+        login_page()
+    else:
         main_page()
-        close_conn()
